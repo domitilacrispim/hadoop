2b154f2cafa4e629199a74741b88771eb952bbae
domitilacrispim <domitilacrispim@gmail.com>
2019-11-15 20:47:00 -0300

    Resposta 5

--- pratica1/map_reducer_py/mapper.py
+++ pratica1/map_reducer_py/mapper.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import sys
 import re
-for line in sys.stdin:
-    line = line.strip()
-    line = line.split('=' )
-    words = line.split('\\' )
-    for word in words:
-     print ('%s\t%s' % (word, 1))
+import fileinput
+import os
+
+for filename in os.listdir("/home/hadoop/hadoop-master/pratica1/map_reducer_py/input/"):
+    for words in open("/home/hadoop/hadoop-master/pratica1/map_reducer_py/input/"+filename):
+     emails = re.findall(r'\d{16}', words)
+     for email in emails:	
+       if(filename!=email):
+        print ('%s\t%s' % (filename,email))
